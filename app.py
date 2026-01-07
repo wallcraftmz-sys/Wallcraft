b06c43280e3c40bd6c80aa26689921abb03a6940
wallcraftmz-sys <wallcraftmz@gmail.com>
2026-01-07 21:16:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -83,6 +83,6 @@ def cart():
         total=total,
         lang=session.get('lang', 'ru')
     )
+     # локальный запуск
 if __name__ == '__main__':
-    # локальный запуск
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)

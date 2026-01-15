eee7f5fb3fdaf68f84213b71db3fb5c45b1259b0
wallcraftmz-sys <wallcraftmz@gmail.com>
2026-01-16 00:55:27 +0200

    Update admin.js

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,5 +1,10 @@
-function deleteProduct(id) {
-    fetch(`/admin/products/delete/${id}`, {
-        method: "POST"
-    }).then(() => location.reload())
+function addProduct() {
+    const data = new FormData(document.getElementById("addForm"))
+
+    fetch("/admin/products/add", {
+        method: "POST",
+        body: data
+    })
+    .then(r => r.json())
+    .then(() => location.reload())
 }

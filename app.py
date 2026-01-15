82d5a91f5058f26f42b4f8ef0e2881f424497af3
wallcraftmz-sys <wallcraftmz@gmail.com>
2026-01-15 13:57:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -74,8 +74,12 @@ app.config.update(
 
 # 🔥 DATABASE (КРИТИЧНО)
 DATABASE_URL = os.getenv("DATABASE_URL")
+
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set")
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
